1cf2b7da46cd9d2576699a089f84338ab501c981
HeitorNunes275 <heitornunes275@gmail.com>
2022-04-13 21:17:13 -0300

    Save, useNavigate 2

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,62 +1,53 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "componentes/MovieCard";
 import Pagination from "componentes/Pagination";
-import { useEffect, useState } from "react";
-import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
-  const [pageNumber, setPageNumber] = useState(0);
 
-  const [page, setPage] = useState<MoviePage>({
-    content: [],
-    last: true,
-    totalPages: 0,
-    totalElements: 0,
-    size: 12,
-    number: 0,
-    first: true,
-    numberOfElements: 0,
-    empty: true,
-  });
+    const [pageNumber, setPageNumber] = useState(0);
 
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
-      .then((response) => {
-        const data = response.data as MoviePage;
-        setPage(data);
-      });
-  }, [pageNumber]);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
-  const handlePageChange = (newPageNumber : number) => {
-    setPageNumber(newPageNumber);
-  }
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
-  return (
-    <>
-      <Pagination page={page} onChange={handlePageChange}/>
-      <div className="container">
-        <div className="row">
-          {page.content.map((movie) => (
-            <div key={movie.id} className="col-sm-6 col-lg-4 col-x1-3 mb-3">
-              <MovieCard movie={movie} />
+    return (
+        <>
+            <Pagination page={page} onChange={handlePageChange} />
+
+            <div className="container">
+                <div className="row">
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
-
-
 export default Listing;
